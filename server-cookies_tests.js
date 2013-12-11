c85faab031555677ae31f38106c57c24e9ccf088
Dave van Soest <dave.soest@gfk.com>
2013-12-11 16:40:39 +0100

    And now it is called version 0.2.0

--- server-cookies_tests.js
+++ server-cookies_tests.js
@@ -1,3 +1,23 @@
 
-// TODO: Write tests.
+if (Meteor.isServer) {
+    Meteor.methods({
+        'test/getCookieByName': function(name) {
+            var cookies = ServerCookies.retrieve(this);
+            return cookies && cookies[name] ? cookies[name] : null;
+        }
+    });
+}
+
+
+testAsyncMulti('server-cookies - cookies should be available on server', (function() {
+    if (Meteor.isClient) {
+        
+    }
+
+    return [
+        function(test, expect) {
+            // TODO!
+        }
+    ];
+})());
 
